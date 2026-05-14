7e78e8ac3896e0171d8affdfb6409c1decbc2c5c
Tim_Yang <tim_yang@asus.com>
2026-05-14 15:57:19 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ async function uploadToImgBB(file) {
   });
   const json = await res.json();
   if (!json.success) throw new Error(json.error?.message || 'Upload failed');
-  return json.data.display_url || json.data.image?.url || json.data.url;
+  return json.data.image?.url || json.data.display_url || json.data.url;
 }
 
 function showUploadStatus(msg) {
